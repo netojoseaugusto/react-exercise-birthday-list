74d5f5e55dc107f4d810efe35780af72c3e82a2a
netojoseaugusto <jose.augusto@sooper.com.br>
2024-10-13 15:10:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import data from "./data";
+import List from "./List";
+
+const App = () => {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <List people={people} />
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => setPeople([])}
+        >
+          clear all
+        </button>
+      </section>
+    </main>
+  );
+};
+export default App;
